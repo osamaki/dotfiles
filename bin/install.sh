f700ba04d24f7d1bc2adddfd8b3fd8f8ea0546c5
Ozaki Ryuichi <ozaki@nlp.ist.i.kyoto-u.ac.jp>
2020-11-05 03:46:18 +0900

    implemented install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,1 +1,13 @@
 # dotfilesへのリンクをホームに貼る
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+	if [[ -e ~/"$f" ]]
+	then
+		echo ~/"$f" already exist
+		continue
+	fi
+    ln -s "$f" ~/"$f"
+	echo linked "$f" to ~/"$f" successfully
+done

705e690c2c6d4d3e6086c3bfd9c474967eb227cb
Ozaki Ryuichi <ozaki@nlp.ist.i.kyoto-u.ac.jp>
2020-11-06 01:20:23 +0900

    fix bug in bin/uninstall.sh

--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -1,15 +1,13 @@
 # dotfilesへのリンクをホームに貼る
 
-cur_dir=$(pwd)
-
 for f in .??*
 do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
 	if [[ -e ~/"$f" ]]
 	then
-		echo unlinked ~/"$f" successfully
 		unlink ~/"$f"
+		echo unlinked ~/"$f" successfully
 	else
 		echo ~/"$f"" doesn't exist"
 	fi

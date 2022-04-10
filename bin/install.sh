9785afc86240fc311078aaa095c8bc3774d07721
osamaki <osamaki@osamakinoMacBook-Air.local>
2022-04-10 20:44:58 +0900

    change to make a link recursively

--- bin/install.sh
+++ bin/install.sh
@@ -4,15 +4,31 @@
 
 cur_dir=$(pwd)
 
+function link_recursively () {
+	echo $1 "(basename: " $(basename $1) " )"
+	base=$(basename $1)
+	[[ "$base" == ".git" ]] && return
+	[[ "$base" == ".DS_Store" ]] && return
+	[[ "$base" == "view" ]] && return
+	
+	# 存在する場合
+	if [[ -e ~/"$1" ]]
+	then
+		echo ~/"$1" already exist
+		if [[ -d "$1" ]]
+		then
+			for f in $(ls "$1"/)
+				link_recursively "$1"/"$f"
+		fi
+		continue
+	else
+		ln -s "$cur_dir"/"$1" ~/"$1"
+		echo linked "$cur_dir"/"$1" to ~/"$1" successfully
+	fi
+}
+
+# .から始まるファイルとディレクトリ
 for f in .??*
 do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
-	if [[ -e ~/"$f" ]]
-	then
-		echo ~/"$f" already exist
-		continue
-	fi
-    ln -s "$cur_dir"/"$f" ~/"$f"
-	echo linked "$cur_dir"/"$f" to ~/"$f" successfully
+	link_recursively "$f"
 done
